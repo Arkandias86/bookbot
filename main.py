2ecebbf4ba1ee5a0f9c6b51113bf20765e251e33
Nico-fox <106523048+Nico-fox@users.noreply.github.com>
2024-04-24 15:38:06 +0200

    added function to count words

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def print_file_contents(fileContents):
     print(fileContents)
 
+def count_lines(fileContents):
+    words = fileContents.split()
+    return len(words)
+
 def main():
     book_path = "./books/frankenstein.txt"
     with open(book_path) as f:
         fileContents = f.read()
-        print_file_contents(fileContents)
+        print(count_lines(fileContents))
 
 if __name__ == "__main__":
     main()
