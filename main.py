d2503345de6ee430d25f8e0b65836465fa05e0b0
Nico-fox <106523048+Nico-fox@users.noreply.github.com>
2024-04-24 15:48:37 +0200

    added function to get the letter frequency

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ def count_lines(fileContents):
     words = fileContents.split()
     return len(words)
 
+def letter_frequency(fileContents):
+    string_lower = fileContents.lower()
+    letters_frequencies = {}
+    for i in range(len(string_lower)):
+        char = string_lower[i]
+        if char in letters_frequencies:
+            letters_frequencies[char] += 1
+        else :
+            letters_frequencies[char] = 1
+    return letters_frequencies
+
 def main():
     book_path = "./books/frankenstein.txt"
     with open(book_path) as f:
         fileContents = f.read()
-        print(count_lines(fileContents))
+        print(letter_frequency(fileContents))
 
 if __name__ == "__main__":
     main()
