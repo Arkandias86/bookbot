9eb918578f9733a12cb21655da694004605e87c4
Nico-fox <106523048+Nico-fox@users.noreply.github.com>
2024-04-24 15:28:45 +0200

    creating function to read the book

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+def print_file_contents(fileContents):
+    print(fileContents)
+
+def main():
+    book_path = "./books/frankenstein.txt"
+    with open(book_path) as f:
+        fileContents = f.read()
+        print_file_contents(fileContents)
+
+if __name__ == "__main__":
+    main()
